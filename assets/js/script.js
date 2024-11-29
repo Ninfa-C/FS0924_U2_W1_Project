f5e0511281f0fb9801c2cf83db71aa890e2079c2
Ninfa-C <ninfa_95@hotmail.it>
2024-11-29 16:32:13 +0100

    768

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
 document.addEventListener('scroll', () => {
     const header = document.querySelector('header');
     const liStarted= document.querySelector('nav ul li:last-child')
-    
-    if (window.scrollY > 485) { // Change '50' to the scroll distance you want
+    const topBanner = document.querySelector('.topBanner')
+    const topBannerRect = topBanner.getBoundingClientRect();
+
+    if (topBannerRect.bottom <= 70) { // Change '50' to the scroll distance you want
         header.classList.add('scrolled');
         liStarted.classList.add('btnScrolled')
     } else {
